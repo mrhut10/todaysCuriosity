485c3fd3f4e79ffdf5e114882cae27965d246a14
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2018-09-03 08:31:29 +1000

    Tidy up code
    Significant code reduction, also ordering things better for readability.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
-
 function loadImage() {
   base_image = new Image();
   base_image.src = 'george.jpg';
   base_image.onload = function(){
     const inputCtx = getCanvasCtxFromSelector('.input-canvas');
     const outputCtx = getCanvasCtxFromSelector('.output-canvas');
-    
+
     paintImageToCanvas(base_image, inputCtx);
     const coolAvatar = makeCoolAvatar(inputCtx);
     paintCoolAvatarToCanvas(coolAvatar, outputCtx);
@@ -13,30 +12,22 @@ function loadImage() {
 }
 
 function paintCoolAvatarToCanvas(imageData, context) {
-  const width = imageData.width;
-  const height = imageData.height;
-  const dutyCycle = 60 / 100;
   const divsX = 12;
   const divsY = 10;
-  const divWidth = width / divsX;
-  const divHeight = height / divsY;
-  const offset = divWidth * 1 / dutyCycle / 2;
-  const dirtyWidth = divWidth * dutyCycle + 5;
-  const dirtyHeight = divHeight * dutyCycle + 5;
-  console.log(offset);
-  context.canvas.width = width * dutyCycle;
-  context.canvas.height = height * dutyCycle;
-  const logger = [];
-  for (let indexX = 0; indexX+1 < width; indexX += divWidth) {
-    for (let indexY = 0; indexY+1 < height; indexY += divHeight) {
-      const dirtyX = indexX;
-      const dirtyY = indexY;
+  const dutyCycle = 60 / 100;
+
+  context.canvas.width = imageData.width * dutyCycle;
+  context.canvas.height = imageData.height * dutyCycle;
+
+  const divWidth = imageData.width / divsX;
+  const divHeight = imageData.height / divsY;
+  for (let indexX = 0; indexX+1 < imageData.width; indexX += divWidth) {
+    for (let indexY = 0; indexY+1 < imageData.height; indexY += divHeight) {
       const dx = indexX * dutyCycle;
       const dy = indexY * dutyCycle ;
-      sanePutImageData(context, imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight);
+      sanePutImageData(context, imageData, dx, dy, indexX, indexY, imageData.width, imageData.height);
     }
   }
-  console.table(logger);
 }
 
 function sanePutImageData(context, imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight) {
@@ -48,9 +39,8 @@ function sanePutImageData(context, imageData, dx, dy, dirtyX, dirtyY, dirtyWidth
 function makeCoolAvatar(context) {
   const width = context.canvas.width;
   const height = context.canvas.height;
-  const coolAvatarData = context.getImageData(0, 0, width, height);
 
-  return messWithPixels(coolAvatarData); // image data of cool avatar
+  return context.getImageData(0, 0, width, height);
 }
 
 function paintImageToCanvas(theImage, context){
@@ -66,16 +56,6 @@ function paintImageToCanvas(theImage, context){
 function getCanvasCtxFromSelector(selector) {
   const inputCanvas = document.querySelector(selector);
   return inputCanvas.getContext('2d');
-}
-
-function messWithPixels(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] - 50; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] + 100; // Blue
-  }
-  
-  return pixels;
 }
 
 loadImage();
