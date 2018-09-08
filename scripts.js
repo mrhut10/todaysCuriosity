ad5b6e8d2a2cb39607db20aadb4aaa8d72e43fbb
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2018-09-08 15:23:58 +1000

    Refactor code
    Instead of garrbing existing canvas elements on the page and
    manipulating them, this moves to a more independent method where
    the script makes it's own canvas elements and returns them so that they
    can be added to the html by the user of this code. More modular and
    re-usable.

--- scripts.js
+++ scripts.js
@@ -2,14 +2,12 @@ function loadImage() {
   base_image = new Image();
   base_image.src = 'george.jpg';
   base_image.onload = function(){
-    const inputCtx = getCanvasCtxFromSelector('.input-canvas');
-    const outputCtx = getCanvasCtxFromSelector('.output-canvas');
-
-    paintImageToCanvas(base_image, inputCtx);
-    // paintCoolAvatarToCanvas(base_image, outputCtx, inputCtx);
-    paintCoolShuffledAvatarToCanvas(base_image, outputCtx, inputCtx);
+    const {inputCanvas, outputCanvas} = reducePixels(base_image, 10, 10, 0.5);
+    document.body.appendChild(inputCanvas);
+    document.body.appendChild(outputCanvas);
   }
 }
+
 
 function paintCoolShuffledAvatarToCanvas(imageData, context, orgCtx) {
   const divsX = 12;
@@ -39,43 +37,47 @@ function paintCoolShuffledAvatarToCanvas(imageData, context, orgCtx) {
   }
 }
 
-function paintCoolAvatarToCanvas(imageData, context, orgCtx) {
-  const divsX = 12;
-  const divsY = 10;
-  const dutyCycle = 60 / 100;
+function reducePixels(baseImage, xDivisions, yDivisions, reductionRatio) {
+  const inputCanvas = document.createElement('canvas');
+  const outputCanvas = document.createElement('canvas');
+  const inputContext = inputCanvas.getContext('2d');
+  const outputContext = outputCanvas.getContext('2d');
+  
+  inputContext.canvas.width = baseImage.width;
+  inputContext.canvas.height = baseImage.height;
+  outputContext.canvas.width = baseImage.width * reductionRatio;
+  outputContext.canvas.height = baseImage.height * reductionRatio;
+  
+  inputContext.drawImage(baseImage, 0, 0);
+  let pixels = inputContext.getImageData(0, 0, inputContext.canvas.width, inputContext.canvas.height);
+  brightPixels = brightenPixels(pixels);
 
-  context.canvas.width = imageData.width * dutyCycle;
-  context.canvas.height = imageData.height * dutyCycle;
-  let brightPixels = orgCtx.getImageData(0, 0, orgCtx.canvas.width, orgCtx.canvas.height);
-  brightPixels = brightenPixels(brightPixels);
+  const divisionWidth = baseImage.width / xDivisions;
+  const divisionHeight = baseImage.height / yDivisions;
+  const sampleWidth = divisionWidth * reductionRatio;
+  const sampleHeight = divisionHeight * reductionRatio;
 
-  const divWidth = imageData.width / divsX;
-  const divHeight = imageData.height / divsY;
-  for (let indexX = 0; indexX+1 < imageData.width; indexX += divWidth) {
-    for (let indexY = 0; indexY+1 < imageData.height; indexY += divHeight) {
-      const dx = indexX * dutyCycle;
-      const dy = indexY * dutyCycle;
-      //draw new image block of pixels
-      context.drawImage(imageData, indexX, indexY, divWidth*dutyCycle, divHeight*dutyCycle, dx, dy, divWidth*dutyCycle+2, divHeight*dutyCycle+2);
+  for (let indexX = 0; indexX+1 < baseImage.width; indexX += divisionWidth) {
+    for (let indexY = 0; indexY+1 < baseImage.height; indexY += divisionHeight) {
+      const dx = indexX * reductionRatio;
+      const dy = indexY * reductionRatio;
       //highlight on the original image
-      orgCtx.putImageData(brightPixels, 0, 0, indexX, indexY, divWidth*dutyCycle, divHeight*dutyCycle)
+      inputContext.putImageData(brightPixels, 0, 0, indexX, indexY, sampleWidth, sampleHeight)
+      //draw new image block of pixels
+      outputContext.drawImage(baseImage, indexX, indexY, sampleWidth, sampleHeight, dx, dy, sampleWidth+2, sampleHeight+2);
     }
   }
+
+  return {inputCanvas, outputCanvas};
 }
 
-function paintImageToCanvas(theImage, context){
-  
+function paintImageToCanvas(theImage, context){  
   const width = theImage.width;
   const height = theImage.height;
   context.canvas.width = width;
   context.canvas.height = height;
   
   context.drawImage(theImage, 0, 0);
-}
-
-function getCanvasCtxFromSelector(selector) {
-  const inputCanvas = document.querySelector(selector);
-  return inputCanvas.getContext('2d');
 }
 
 function brightenPixels({width, height, data}) {
