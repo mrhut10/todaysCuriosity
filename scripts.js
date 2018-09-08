4d676bd8848254cd0fc8d1865cf25c1ff7191000
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2018-09-08 21:23:01 +1000

    Move reversePixels into class

--- scripts.js
+++ scripts.js
@@ -4,7 +4,8 @@ function loadImage() {
   base_image.onload = function(){
     const curiosity = new todaysCuriosity(base_image);
     curiosity.setDivisions(10, 10, 0.5);
-    const {inputCanvas, outputCanvas} = curiosity.reducePixels();
+    // const {inputCanvas, outputCanvas} = curiosity.reducePixels();
+    const {inputCanvas, outputCanvas} = curiosity.reversePixels();
 
     document.body.appendChild(inputCanvas);
     document.body.appendChild(outputCanvas);
--- todays-curiosity.js
+++ todays-curiosity.js
@@ -45,6 +45,24 @@ class todaysCuriosity {
     this.outputContext.drawImage(this.baseImage, indexX, indexY, sampleWidth, sampleHeight, dx, dy, sampleWidth+2, sampleHeight+2);
   }
 
+  reversePixels(){
+    this.loopThroughImageDivisions(this.paintReverseBlocks.bind(this));
+    
+    return {
+      inputCanvas : this.inputCanvas,
+      outputCanvas : this.outputCanvas,
+    };  
+  }
+
+  paintReverseBlocks( indexX, indexY, sampleWidth, sampleHeight ) {
+    const dx = indexX * this.reductionRatio;
+    const dy = indexY * this.reductionRatio;
+    const reverseIndexX = this.baseImage.width - indexX - sampleWidth;
+    const reverseIndexY = this.baseImage.height - indexY - sampleHeight;
+    //draw new image block of pixels
+    this.outputContext.drawImage(this.baseImage, reverseIndexX, reverseIndexY, sampleWidth, sampleHeight, dx, dy, sampleWidth+2, sampleHeight+2);
+  }
+
   loopThroughImageDivisions(fn) {
     const divisionWidth = this.baseImage.width / this.xDivisions;
     const divisionHeight = this.baseImage.height / this.yDivisions;
@@ -58,43 +76,7 @@ class todaysCuriosity {
     }
   }
 };
-  
-function paintCoolShuffledAvatarToCanvas(imageData, context, orgCtx) {
-  const divsX = 12;
-  const divsY = 10;
-  const dutyCycle = 100 / 100;
-  
-  context.canvas.width = imageData.width * dutyCycle;
-  context.canvas.height = imageData.height * dutyCycle;
-  let brightPixels = orgCtx.getImageData(0, 0, orgCtx.canvas.width, orgCtx.canvas.height);
-  brightPixels = brightenPixels(brightPixels);
-  
-  const divWidth = imageData.width / divsX;
-  const divHeight = imageData.height / divsY;
-  for (let indexX = 0; indexX+1 < imageData.width; indexX += divWidth) {
-    for (let indexY = 0; indexY+1 < imageData.height; indexY += divHeight) {
-      const dx = indexX * dutyCycle;
-      const dy = indexY * dutyCycle;
-      const reverseIndexX = imageData.width - indexX - divWidth;
-      const reverseIndexY = imageData.height - indexY - divHeight;
-      //draw new image block of pixels
-      context.drawImage(imageData, reverseIndexX, reverseIndexY, divWidth*dutyCycle, divHeight*dutyCycle, dx, dy, divWidth*dutyCycle+2, divHeight*dutyCycle+2);
-      //highlight on the original image
-      orgCtx.putImageData(brightPixels, 0, 0, indexX, indexY, divWidth*dutyCycle, divHeight*dutyCycle)
-    }
-  }
-}
 
-
-
-function paintImageToCanvas(theImage, context){  
-  const width = theImage.width;
-  const height = theImage.height;
-  context.canvas.width = width;
-  context.canvas.height = height;
-  
-  context.drawImage(theImage, 0, 0);
-}
 
 function brightenPixels({width, height, data}) {
   return new ImageData(
