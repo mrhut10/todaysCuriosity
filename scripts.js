94553b0622f33005ae64cefc012ee4f6d0b52592
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2018-09-16 15:17:54 +1000

    Load default image
    The first instance of todaysCuriosity is created when the deault image
    is loaded, all other image loads use the original instance (stops double
    ups of images)

--- scripts.js
+++ scripts.js
@@ -1,35 +1,45 @@
 import loadImage from "blueimp-load-image";
 import todaysCuriosity from './todays-curiosity';
-//const todaysCuriosity = require('./todays-curiosity');
+import george from './george.jpg';
 
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
   // alert('file api works')
 } else {
-  alert('Oh no, looks like your browser might be a bit old for this app to work. Maybe try the lates Chrome or Firefox. and call ghast busters');
+  alert('Oh no, looks like your browser might be a bit old for this app to work. Maybe try the latest Chrome or Firefox.');
 }
 
-const curiosity = new todaysCuriosity(new Image(20,20));
+let curiosity;
+
+const defaultImage = new Image();
+defaultImage.src = george;
+defaultImage.onload = function() {
+  console.log('onload', defaultImage);
+  curiosity = new todaysCuriosity(defaultImage);
+  imageSetup(defaultImage);
+};
 
 document.getElementById('file-input').onchange = function (e) {
   loadImage(
       e.target.files[0],
-      function (img) {
-        curiosity.baseImage = img;
-        curiosity.paintInputImage();
-        curiosity.createBrightpixels();
-        // const {inputCanvas, outputCanvas} = curiosity.getReducedPixelBlocks();
-        const {inputCanvas, outputCanvas} = curiosity.getReversedPixelBlocks();
-    
-        const inputDiv = document.getElementById('input-display');
-        inputDiv.appendChild(inputCanvas);
-        const outputDiv = document.getElementById('output-display');
-        outputDiv.appendChild(outputCanvas);
-        
-        listenerStuff(curiosity);
-      },
+      imageSetup,
       {maxWidth: 2000} // Options
   );
+};
+
+function imageSetup (img) {
+  curiosity.baseImage = img;
+  curiosity.paintInputImage();
+  curiosity.createBrightpixels();
+  // const {inputCanvas, outputCanvas} = curiosity.getReducedPixelBlocks();
+  const {inputCanvas, outputCanvas} = curiosity.getReversedPixelBlocks();
+
+  const inputDiv = document.getElementById('input-display');
+  inputDiv.appendChild(inputCanvas);
+  const outputDiv = document.getElementById('output-display');
+  outputDiv.appendChild(outputCanvas);
+  
+  listenerStuff(curiosity);
 };
 
 
