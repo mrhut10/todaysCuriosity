e9b534d681bba52204e092c3bd2559582d2a64cc
David Pham <david.pham@medela.ca>
2018-10-15 13:49:42 -0400

    Added footer avatars

--- footer.js
+++ footer.js
@@ -13,37 +13,51 @@ request.send();
 request.onload = function() {
   request.response.forEach(contributor => {
     contributors.push({
+      avatar_url: contributor.avatar_url,
       login: contributor.login,
       html_url: contributor.html_url,
       contributions: contributor.contributions
     });
   });
 
-  console.log(contributors);
-  const footerContents = document.createElement("p");
-  footerContents.append("A project by ");
+  const footerText = document.createElement("p");
+  footerText.append("A project by ");
+
+  const footerAvatar = document.createElement("p");
 
   contributors.forEach((contributor, index) => {
+    // footer text
     const contributorElement = document.createElement("a");
     contributorElement.append(
       contributor.login + "(" + contributor.contributions + ")"
     );
     contributorElement.setAttribute("href", contributor.html_url);
 
-    console.log(contributors.length, index);
-
-    footerContents.appendChild(contributorElement);
+    footerText.appendChild(contributorElement);
 
     if (contributors.length - 2 === index) {
-      footerContents.append(" and ");
+      footerText.append(" and ");
     } else if (contributors.length - 2 >= index) {
-      footerContents.append(", ");
+      footerText.append(", ");
     }
+
+    // footer avatar
+    const avatarLink = document.createElement("a");
+    avatarLink.setAttribute("href", contributor.html_url);
+    const avatarImg = document.createElement("img");
+    avatarImg.setAttribute("src", contributor.avatar_url);
+    avatarImg.setAttribute("height", "40px");
+    avatarImg.setAttribute("width", "40px");
+
+    avatarLink.append(avatarImg);
+
+    footerAvatar.append(avatarLink);
   });
 
-  footerContents.append(".");
+  footerText.append(".");
 
   const footer = document.querySelector("#footer");
 
-  footer.append(footerContents);
+  footer.append(footerAvatar);
+  footer.append(footerText);
 };
